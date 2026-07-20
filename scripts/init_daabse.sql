1e5bffc7d93f9be9f074b058bee75c4cad65dab1
mehdisaghiri <saghirimehdi12@gmail.com>
2026-07-20 17:29:46 +0100

    database creation and schemas

--- scripts/init_daabse.sql
+++ scripts/init_daabse.sql
@@ -1,22 +1,45 @@
-/*
-this script for creating the databse 
-1- move to master 
-2- create the databse
-3- create the schemas 
+/* ============================================================
+   Project: Car Sales Data Warehouse
+   Purpose: Recreate database and schemas
+   Warning: This deletes the existing database and all its data
+   ============================================================ */
 
-*/
+USE master;
+GO
 
-USE master ;
+/* Drop the database if it already exists */
+IF DB_ID(N'car_sales_dwh') IS NOT NULL
+BEGIN
+    ALTER DATABASE car_sales_dwh
+    SET SINGLE_USER
+    WITH ROLLBACK IMMEDIATE;
 
+    DROP DATABASE car_sales_dwh;
+END;
+GO
+
+/* Create the data warehouse database */
 CREATE DATABASE car_sales_dwh;
+GO
 
 USE car_sales_dwh;
+GO
 
+/* Create the Medallion architecture schemas */
 CREATE SCHEMA bronze;
 GO
+
 CREATE SCHEMA silver;
 GO
+
 CREATE SCHEMA gold;
 GO
 
-
+/* Validate database and schema creation */
+SELECT
+    DB_NAME() AS database_name,
+    name AS schema_name
+FROM sys.schemas
+WHERE name IN ('bronze', 'silver', 'gold')
+ORDER BY name;
+GO
